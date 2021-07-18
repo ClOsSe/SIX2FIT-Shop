65277fd29cfe7428cc5ea9fa5f0c585f34688c51
shayan azadbakht <shayanazadbakht1781@gmail.com>
2021-07-18 16:11:50 +0430

    add card_with_pic_and_dec

---
+++ src/components/card_with_pic_and_dec/CardWithPictureAndDescription.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row q-col-gutter-lg">
+   <template v-for="item in 3" :key="item">
+        <div class="col-12 col-sm-4 col-md-4">
+      <q-card class="my-card text-center" flat bordered>
+        <q-icon name="home" />
+
+        <q-card-section class="items-stretch flex-center justify-between ">
+          <div class="text-overline text-orange-9">Overline</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+          <div class="text-caption text-grey">
+            {{ title }}
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+   </template>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+        }
+    }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,30 @@
 <template>
-  <q-page class="flex ">
-    <div>
-      <!-- <Slider /> -->
+  <q-page class="flex q-page col-12" >
+    <div class="fit">
+      <Slider />
       <ThumbnailSlider />
+      <SingleBanner />
+      <CardWithPictureAndDescription />
       <h1>این صفحه خانه می‌باشد.</h1>
     </div>
   </q-page>
 </template>
 
 <script>
-// import Slider from '../components/width_slider/MainSlider.vue'
+import Slider from '../components/width_slider/MainSlider.vue'
 import ThumbnailSlider from '../components/thumbnail_slider/ThumbnailSlider.vue'
+import SingleBanner from '../components/single_banner/Banner.vue'
+import CardWithPictureAndDescription from 'src/components/card_with_pic_and_dec/CardWithPictureAndDescription.vue';
 
 export default {
   name: "PageIndex",
   components:{
-    // Slider,
-    ThumbnailSlider
+    Slider,
+    ThumbnailSlider,
+    SingleBanner,
+    CardWithPictureAndDescription
+
   }
 };
 </script>
+
