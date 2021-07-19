b011dda5bbb2c9f1f530c9f881ab4eba6f36a2d5
mohammad <webmaster.webdeveloper@gmail.com>
2021-07-19 04:54:38 -0700

    register page path (route) created

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,30 +1,73 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Index.vue'),name:"Home" },
-      { path: 'AboutUs', component: () => import('pages/about_us/AboutUs.vue'),name:"AboutUs" },
-      { path: 'Article', component: () => import('pages/articles/Article.vue'),name:"Article" },
-      { path: 'Cart', component: () => import('pages/cart/Cart.vue'),name:"Cart" },
-      { path: 'ContactUs', component: () => import('pages/contact_us/ContactUs.vue'),name:"ContactUs" },
-      { path: 'FAQ', component: () => import('pages/faq/FAQ.vue'),name:"FAQ" },
-      { path: 'Payment', component: () => import('pages/payment/Payment.vue'),name:"Payment" },
-      { path: 'Rules', component: () => import('pages/rules/Rules.vue'),name:"Rules" },
-      { path: 'SingleArticle', component: () => import('pages/single_article/SingleArticle.vue'),name:"SingleArticle" },
-      { path: 'Store', component: () => import('pages/store/Store.vue'),name:"Store" },
-      { path: 'Ticket', component: () => import('pages/ticket/Ticket.vue'),name:"Ticket" },
-      
-    ]
+      { path: "", component: () => import("pages/Index.vue"), name: "Home" },
+      {
+        path: "AboutUs",
+        component: () => import("pages/about_us/AboutUs.vue"),
+        name: "AboutUs",
+      },
+      {
+        path: "Article",
+        component: () => import("pages/articles/Article.vue"),
+        name: "Article",
+      },
+      {
+        path: "Cart",
+        component: () => import("pages/cart/Cart.vue"),
+        name: "Cart",
+      },
+      {
+        path: "ContactUs",
+        component: () => import("pages/contact_us/ContactUs.vue"),
+        name: "ContactUs",
+      },
+      {
+        path: "FAQ",
+        component: () => import("pages/faq/FAQ.vue"),
+        name: "FAQ",
+      },
+      {
+        path: "Payment",
+        component: () => import("pages/payment/Payment.vue"),
+        name: "Payment",
+      },
+      {
+        path: "Rules",
+        component: () => import("pages/rules/Rules.vue"),
+        name: "Rules",
+      },
+      {
+        path: "SingleArticle",
+        component: () => import("pages/single_article/SingleArticle.vue"),
+        name: "SingleArticle",
+      },
+      {
+        path: "Store",
+        component: () => import("pages/store/Store.vue"),
+        name: "Store",
+      },
+      {
+        path: "Ticket",
+        component: () => import("pages/ticket/Ticket.vue"),
+        name: "Ticket",
+      },
+      {
+        path: "register-vendors",
+        component: () => import("pages/register/RegisterVendors.vue"),
+        name: "RegisterVendors",
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
 
-export default routes
+export default routes;
