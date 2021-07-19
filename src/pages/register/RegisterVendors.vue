22dbf1617dae4f6154a610a2c87b48acdc643b3b
mohammad <webmaster.webdeveloper@gmail.com>
2021-07-19 04:53:41 -0700

    register page created

---
+++ src/pages/register/RegisterVendors.vue
@@ -0,0 +1,10 @@
+<template>
+  <RegisterForm />
+</template>
+<script>
+import RegisterForm from "../../components/register/Register.vue";
+export default {
+  components: { RegisterForm },
+};
+</script>
+<style lang="scss" scoped></style>
