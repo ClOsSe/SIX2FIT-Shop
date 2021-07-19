2e48f32aa04b491853829ae643d1257e8e4134d6
mohammad <webmaster.webdeveloper@gmail.com>
2021-07-18 21:35:36 -0700

    footer fixed to center

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
     <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          <div>Title</div>
-        </q-toolbar-title>
-      </q-toolbar>
+      <div class="text-center justify-center">
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+      </div>
     </q-footer>
   </div>
 </template>
