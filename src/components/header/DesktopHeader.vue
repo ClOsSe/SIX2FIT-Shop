72595a8c12effb3b4f2a3635a7fd31bd41b383bc
shayan azadbakht <shayanazadbakht1781@gmail.com>
2021-07-21 12:50:14 +0430

    change header layout

--- src/components/header/DesktopHeader.vue
+++ src/components/header/DesktopHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="q-pa-md">
-    <q-header
+  <div class="">
+    <div
       style="background: #4527a0 !important"
       bg-color="deep-purple-10"
       elevated
     >
-      <div class="row">
+      <div class="row q-pa-sm">
         <!-- logo -->
         <q-toolbar class="col-4">
           <router-link :to="{ name: 'Home' }">
@@ -60,7 +60,7 @@
         <q-tab name="contactus" label="تماس با ما" />
         <q-tab name="vendors" label="فروشندگان" />
       </q-tabs>
-    </q-header>
+    </div>
   </div>
 </template>
 <script>
